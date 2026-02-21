d4cae9f494af2698f1328a5046ed30fde61e4239
saige.kumar <saige.kumar@GMR-118-2-3>
2026-02-21 15:57:01 -0800

    done

---
+++ NeuralNetworkAssignments/NeuralNetworkAssignments/ActivationFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorePerceptron
+{
+    public class ActivationFunction
+    {
+        public Func<double, double> function;
+        public Func<double, double> derivative;
+
+        public ActivationFunction(Func<double, double> func, Func<double, double> deriv)
+        {
+            function = func;
+            derivative = deriv;
+        }
+        public static double test(double x)
+        {
+            double firstE = Math.Pow(Math.E, x);
+            double secondE = Math.Pow(Math.E, -x);
+            double numerator = firstE - secondE;
+            double denominator = firstE + secondE;
+            double final = numerator / denominator;
+            return final;
+        }
+    }
+    //(Math.Pow(Math.E, x) - Math.Pow(Math.E, -x)) / (Math.Pow(Math.E, x) + Math.Pow(Math.E, -x))
+    public static class ActivationFunctions
+    {
+        public static ActivationFunction Tanh => new ActivationFunction((x) => ( ActivationFunction.test(x)), 
+                                                      (x) => ( 1 - Math.Pow(((Math.Pow(Math.E, x) - Math.Pow(Math.E, -x)) / (Math.Pow(Math.E, x) + Math.Pow(Math.E, -x))), 2) ));
+        public static ActivationFunction Sigmoid => new ActivationFunction((x) => ( 1 / (1 + Math.Pow(Math.E, -x)) ), 
+                                (x) => ( (1 / (1 + Math.Pow(Math.E, -x))) * (1 - (1 / (1 + Math.Pow(Math.E, -x)))) ) );
+    }
+
+}
---
+++ NeuralNetworkAssignments/NeuralNetworkAssignments/ErrorFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorePerceptron
+{
+    public class ErrorFunction
+    {
+        public Func<double, double, double> function;
+        public Func<double, double, double> derivative;
+
+        public ErrorFunction(Func<double, double, double> func, Func<double, double, double> deriv)
+        {
+            function = func;
+            derivative = deriv;
+        }
+    }
+
+    public static class ErrorFunctions
+    {
+        public static ErrorFunction MAE => new ErrorFunction((x, y) => Math.Abs(x - y), (x, y) => y - x > 0 ? -1 : 1);
+        public static ErrorFunction MSE => new ErrorFunction((x, y) => (x - y) * (x - y), (x, y) => -2 * (x - y));
+    }
+}
